a117245fafc27106c1ed68cc27da515bf9b7be3f
cicero1020 <ciceroamorim10igta10@gmail.com>
2023-03-06 12:07:33 -0300

    Projeto python oficial V1

--- api_cotacao_moeda.py
+++ api_cotacao_moeda.py
@@ -1,1 +1,19 @@
-Bem vindo ao git v1
+import requests
+
+
+try:
+    var=input("Imforme um cep ?")
+     while len(var) < 8:
+    
+    
+
+    
+
+
+    url=f'https://viacep.com.br/ws/${cep}/json/'
+    result=requests.get(url)
+    requisicao=result.json()
+    print(requisicao["cep"])
+except:
+    print("Ops: algo deu errado !!!")
+
